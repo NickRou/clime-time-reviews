e6172a3d6e73810a38ca7d641d1c86e53a630f82
Nicklas Rouvinen <nicklas.rouvinen@gmail.com>
2024-09-30 20:40:21 -0700

    add login page and supabase client

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Nav() {
   return (
     <header className="bg-white">
@@ -5,20 +7,24 @@ export default function Nav() {
         <div className="flex flex-col items-start gap-4 md:flex-row md:items-center md:justify-between">
           <div>
             <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
-              Clime Time Reviews
+              <Link href="/">Clime Time Reviews</Link>
             </h1>
-
             <p className="mt-1.5 text-sm text-gray-500">
               Reviews by your friends
             </p>
           </div>
-
           <div className="flex items-center gap-4">
+            <button
+              className="inline-block rounded px-5 py-3 text-sm font-medium text-black transition hover:bg-gray-100 focus:outline-none focus:ring"
+              type="button"
+            >
+              <Link href="/writeareview">Write A Review</Link>
+            </button>
             <button
               className="inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
               type="button"
             >
-              Login
+              <Link href="/login">Login</Link>
             </button>
           </div>
         </div>
---
+++ app/writeareview/page.tsx
@@ -0,0 +1,9 @@
+import Nav from "../components/Nav";
+
+export default async function Page() {
+  return (
+    <div className="flex flex-col h-screen">
+      <Nav />
+    </div>
+  );
+}
