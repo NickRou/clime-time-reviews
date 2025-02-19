bb9a232cb6b9dc97d940ba2c25e9f03f292d1bbf
Nicklas Rouvinen <nicklas.rouvinen@gmail.com>
2025-02-18 18:39:56 -0800

    fix popups to use react-map-gl wrapper

--- src/components/MapBoxMap.tsx
+++ src/components/MapBoxMap.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-import Map, { Marker } from 'react-map-gl/mapbox'
+import Map, { Marker, Popup } from 'react-map-gl/mapbox'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { PostWithUser } from '@/lib/types'
 import { renderToString } from 'react-dom/server'
 import MapBoxMapPopup from './MapBoxMapPopup'
+import { useState } from 'react'
 
 interface MapBoxMapProps {
   posts: PostWithUser[]
 }
 
 export default function MapBoxMap({ posts }: MapBoxMapProps) {
+  const [selectedPost, setSelectedPost] = useState<PostWithUser | null>(null)
+
   if (!process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN) {
     throw new Error('Mapbox access token is not set')
   }
@@ -53,20 +56,28 @@ export default function MapBoxMap({ posts }: MapBoxMapProps) {
           longitude={post.loc_longitude}
           latitude={post.loc_latitude}
           color="red"
-          popup={new mapboxgl.Popup({
-            maxWidth: '400px',
-            closeButton: false,
-            anchor: 'bottom',
-            offset: [0, -25],
-          }).setHTML(
-            `
-              <div>
-                ${renderToString(<MapBoxMapPopup post={post} />)}
-              </div>
-            `
-          )}
+          onClick={(e) => {
+            e.originalEvent.stopPropagation()
+            setSelectedPost(
+              selectedPost?.post_id === post.post_id ? null : post
+            )
+          }}
         />
       ))}
+
+      {selectedPost && (
+        <Popup
+          longitude={selectedPost.loc_longitude}
+          latitude={selectedPost.loc_latitude}
+          anchor="bottom"
+          onClose={() => setSelectedPost(null)}
+          maxWidth="400px"
+          closeButton={false}
+          offset={[0, -25]}
+        >
+          <MapBoxMapPopup post={selectedPost} />
+        </Popup>
+      )}
     </Map>
   )
 }
